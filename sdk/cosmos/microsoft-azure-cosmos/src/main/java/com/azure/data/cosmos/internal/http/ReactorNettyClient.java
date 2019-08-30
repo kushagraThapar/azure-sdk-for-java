7b7a6200c49ebcd808e9b7edb4aefff9768af182
Kushagra Thapar <kuthapar@microsoft.com>
2019-08-30 10:46:04 -0700

    Reactor netty improvements, added idle state handler, connection pool reduced to 500

--- sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/http/ReactorNettyClient.java
+++ sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/http/ReactorNettyClient.java
@@ -4,8 +4,16 @@ package com.azure.data.cosmos.internal.http;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.logging.LogLevel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +26,8 @@ import reactor.netty.http.client.HttpClientRequest;
 import reactor.netty.http.client.HttpClientResponse;
 import reactor.netty.resources.ConnectionProvider;
 import reactor.netty.tcp.ProxyProvider;
-import reactor.netty.tcp.TcpResources;
 
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Objects;
 import java.util.function.BiFunction;
@@ -31,30 +39,84 @@ import static com.azure.data.cosmos.internal.http.HttpClientConfig.REACTOR_NETWO
  */
 class ReactorNettyClient implements HttpClient {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass().getSimpleName());
+    private static final Logger logger = LoggerFactory.getLogger(ReactorNettyClient.class.getSimpleName());
 
     private HttpClientConfig httpClientConfig;
     private reactor.netty.http.client.HttpClient httpClient;
     private ConnectionProvider connectionProvider;
 
+    private ReactorNettyClient() {}
+
+    /**
+     * Creates ReactorNettyClient with un-pooled connection.
+     */
+    public static ReactorNettyClient create(HttpClientConfig httpClientConfig) {
+        ReactorNettyClient reactorNettyClient = new ReactorNettyClient();
+        reactorNettyClient.httpClientConfig = httpClientConfig;
+        reactorNettyClient.httpClient = reactor.netty.http.client.HttpClient.newConnection();
+        reactorNettyClient.configureChannelPipelineHandlers();
+        return reactorNettyClient;
+    }
+
     /**
      * Creates ReactorNettyClient with {@link ConnectionProvider}.
      */
-    ReactorNettyClient(ConnectionProvider connectionProvider, HttpClientConfig httpClientConfig) {
-        this.connectionProvider = connectionProvider;
-        this.httpClientConfig = httpClientConfig;
-        this.httpClient = reactor.netty.http.client.HttpClient.create(connectionProvider);
-        configureChannelPipelineHandlers();
+    public static ReactorNettyClient createWithConnectionProvider(ConnectionProvider connectionProvider, HttpClientConfig httpClientConfig) {
+        ReactorNettyClient reactorNettyClient = new ReactorNettyClient();
+        reactorNettyClient.connectionProvider = connectionProvider;
+        reactorNettyClient.httpClientConfig = httpClientConfig;
+        reactorNettyClient.httpClient = reactor.netty.http.client.HttpClient.create(connectionProvider);
+        reactorNettyClient.configureChannelPipelineHandlers();
+        return reactorNettyClient;
     }
 
     private void configureChannelPipelineHandlers() {
         this.httpClient = this.httpClient.tcpConfiguration(tcpClient -> {
-            if (LoggerFactory.getLogger(REACTOR_NETWORK_LOG_CATEGORY).isTraceEnabled()) {
-                tcpClient = tcpClient.wiretap(REACTOR_NETWORK_LOG_CATEGORY, LogLevel.TRACE);
-            }
             if (this.httpClientConfig.getProxy() != null) {
-                tcpClient = tcpClient.proxy(typeSpec -> typeSpec.type(ProxyProvider.Proxy.HTTP).address(this.httpClientConfig.getProxy()));
+                tcpClient =
+                    tcpClient.proxy(typeSpec -> typeSpec.type(ProxyProvider.Proxy.HTTP).address(this.httpClientConfig.getProxy()));
             }
+            tcpClient =
+                tcpClient.secure(sslContextSpec -> sslContextSpec.sslContext(this.httpClientConfig.getConfigs().getSslContext()));
+            if (LoggerFactory.getLogger(REACTOR_NETWORK_LOG_CATEGORY).isTraceEnabled()) {
+                tcpClient = tcpClient.wiretap(REACTOR_NETWORK_LOG_CATEGORY, LogLevel.INFO);
+            }
+            //  By default, keep alive is enabled on http client
+            tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30 * 1000);
+
+            tcpClient = tcpClient.doOnConnected(connection -> {
+
+                final ChannelPipeline pipeline = connection.channel().pipeline();
+
+                if (pipeline.context("httpObjectAggregator") == null) {
+                    pipeline.addLast("httpObjectAggregator", new HttpObjectAggregator(this.httpClientConfig
+                        .getConfigs().getMaxHttpBodyLength()));
+                }
+
+                if (this.httpClientConfig.getMaxIdleConnectionTimeoutInMillis() != null) {
+                    //  Note: This is an open issue and will be supported in Project Reactor v3.3.0
+                    //  https://github.com/reactor/reactor-netty/releases/tag/v0.9.0.M3
+
+                    //  Note: This is an alternative approach suggested on this issue:
+                    //  https://github.com/reactor/reactor-netty/issues/612
+                    int maxIdleConnectionTimeoutInSeconds =
+                        this.httpClientConfig.getMaxIdleConnectionTimeoutInMillis() / 1000;
+
+                    int timeoutInSeconds = 0;
+                    if (this.httpClientConfig.getRequestTimeoutInMillis() != null) {
+                        timeoutInSeconds = this.httpClientConfig.getRequestTimeoutInMillis() / 1000;
+                    }
+
+                    if (pipeline.context("idleStateHandler") == null) {
+                        pipeline.addLast("idleStateHandler", new IdleStateHandler(timeoutInSeconds, timeoutInSeconds,
+                            maxIdleConnectionTimeoutInSeconds));
+                    }
+
+                    if (pipeline.context("idleCleanupHandler") == null) {
+                        pipeline.addLast("idleCleanupHandler", new IdleCleanupHandler());
+                    }
+                }
+            });
             return tcpClient;
         });
     }
@@ -66,6 +128,7 @@ class ReactorNettyClient implements HttpClient {
         Objects.requireNonNull(this.httpClientConfig);
 
         return this.httpClient
+                .keepAlive(this.httpClientConfig.isConnectionKeepAlive())
                 .port(request.port())
                 .request(HttpMethod.valueOf(request.httpMethod().toString()))
                 .uri(request.uri().toString())
@@ -87,9 +150,9 @@ class ReactorNettyClient implements HttpClient {
             }
             if (restRequest.body() != null) {
                 Flux<ByteBuf> nettyByteBufFlux = restRequest.body().map(Unpooled::wrappedBuffer);
-                return reactorNettyOutbound.options(sendOptions -> sendOptions.flushOnEach(false)).send(nettyByteBufFlux);
+                return reactorNettyOutbound.send(nettyByteBufFlux);
             } else {
-                return reactorNettyOutbound.options(sendOptions -> sendOptions.flushOnEach(false));
+                return reactorNettyOutbound;
             }
         };
     }
@@ -107,8 +170,9 @@ class ReactorNettyClient implements HttpClient {
 
     @Override
     public void shutdown() {
-        TcpResources.disposeLoopsAndConnections();
-        this.connectionProvider.dispose();
+        if (this.connectionProvider != null) {
+            this.connectionProvider.dispose();
+        }
     }
 
     private static class ReactorNettyHttpResponse extends HttpResponse {
@@ -143,6 +207,11 @@ class ReactorNettyClient implements HttpClient {
         }
 
         @Override
+        public Flux<InputStream> bodyAsInputStream() {
+            return bodyIntern().asInputStream().doFinally(s -> this.close());
+        }
+
+        @Override
         public Mono<byte[]> bodyAsByteArray() {
             return bodyIntern().aggregate().asByteArray().doFinally(s -> this.close());
         }
@@ -160,6 +229,7 @@ class ReactorNettyClient implements HttpClient {
         @Override
         public void close() {
             if (reactorNettyConnection.channel().eventLoop().inEventLoop()) {
+                logger.info("Is Persistent {}", reactorNettyConnection.isPersistent());
                 reactorNettyConnection.dispose();
             } else {
                 reactorNettyConnection.channel().eventLoop().execute(reactorNettyConnection::dispose);
@@ -173,6 +243,24 @@ class ReactorNettyClient implements HttpClient {
         @Override
         Connection internConnection() {
             return reactorNettyConnection;
+        }
+    }
+
+    static class IdleCleanupHandler extends ChannelDuplexHandler {
+
+        @Override
+        public void userEventTriggered(final ChannelHandlerContext ctx, final Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent) {
+                final IdleState state = ((IdleStateEvent) evt).state();
+                if (state == IdleState.ALL_IDLE
+                    || state == IdleState.READER_IDLE
+                    || state == IdleState.WRITER_IDLE) {
+                    // close idle channel
+                    ctx.flush().close();
+                }
+            } else {
+                super.userEventTriggered(ctx, evt);
+            }
         }
     }
 }
